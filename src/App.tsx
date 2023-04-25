b98a875aa309396be697288b4bd8422037abd92f
cris-dangithub <cristiandaniel8080@gmail.com>
2023-04-25 15:47:04 -0500

    renamed files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import "./App.css";
+import Navbar from "./shared/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <nav>*-----NAVBAR-----*</nav>
       <main>*MAIN_CONTENT_PAGES*</main>
       <footer>*-----FOOTER-----*</footer>
--- src/shared/Navbar.tsx
+++ src/shared/Navbar.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import LogoBlue from "../assets/svg/LogoBlue";
+import LogoWhite from "../assets/svg/LogoWhite";
 
 const Navbar = () => {
-  return <nav className="c-navbar"></nav>;
+  return (
+    <nav className="c-navbar">
+      <LogoWhite />
+    </nav>
+  );
 };
 
 export default Navbar;
