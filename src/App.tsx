d93731908c6739c5bac692c9d73ec84027257f53
cris-dangithub <cristiandaniel8080@gmail.com>
2023-04-25 14:52:20 -0500

    app-file-structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <nav>*-----NAVBAR-----*</nav>
+      <main>*MAIN_CONTENT_PAGES*</main>
+      <footer>*-----FOOTER-----*</footer>
+    </>
+  );
 }
 
 export default App;
